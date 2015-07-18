349e3895e4e9507c692fc9c1ed189fe60323decd
davidyin1212 <davidyin1212@gmail.com>
2015-07-18 12:21:29 -0400

    newlines filter

--- controllers/ExpirenceController.js
+++ controllers/ExpirenceController.js
@@ -35,4 +35,12 @@ angular.module('portfolioApp').filter('newlines', function () {
     return function(text) {
         return text.replace(/\n/g, '<br/>');
     }
+})
+.filter('noHTML', function () {
+    return function(text) {
+        return text
+                .replace(/&/g, '&amp;')
+                .replace(/>/g, '&gt;')
+                .replace(/</g, '&lt;');
+    }
 });
