a361404153ad78418a33a5a780b841abaa65b1c2
davidyin1212 <davidyin1212@gmail.com>
2015-09-12 16:36:44 -0400

    add click arrow action

--- scroll.js
+++ scroll.js
@@ -17,18 +17,3 @@ $(window).scroll( function(){
   $('.arrow-wrap').css('opacity', position);
 
 });
-//Code stolen from css-tricks for smooth scrolling:
-$(function() {
-  $('a[href*=#]:not([href=#])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html,body').animate({
-          scrollTop: target.offset().top
-        }, 1000);
-        return false;
-      }
-    }
-  });
-});
